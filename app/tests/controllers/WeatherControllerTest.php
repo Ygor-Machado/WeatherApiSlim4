68bad2132c70056fdd00f67c5a8fdd5124d280c7
Ygor-Machado <ygaounderground011@gmail.com>
2024-02-02 11:55:06 -0300

    feat: adiciona teste WeatherController

--- app/tests/controllers/WeatherControllerTest.php
+++ app/tests/controllers/WeatherControllerTest.php
@@ -2,7 +2,11 @@
 
 namespace app\tests\controllers;
 
+use app\controllers\WeatherController;
+use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 
 class WeatherControllerTest extends TestCase
@@ -10,9 +14,48 @@ class WeatherControllerTest extends TestCase
 
     // Não consegui fazer funcionar esse teste
 
-    public function testIndex()
+    /**
+     * @throws Exception
+     */
+    public function testIndexSuccess()
     {
+        $weatherControllerMock = $this->createMock(WeatherController::class);
 
+        $request = $this->createMock(ServerRequestInterface::class);
+        $request->method('getQueryParams')->willReturn(['city' => 'Franca']);
+
+        $response = $this->createMock(ResponseInterface::class);
+
+        $weatherControllerMock->expects($this->once())
+            ->method('index')
+            ->willReturnCallback(function ($request, $response) {
+                return ['location' => ['name' => 'Franca'], 'current' => ['condition_icon' => 'icon']];
+            });
+
+        $result = $weatherControllerMock->index($request, $response);
+
+        $this->assertEquals('Franca', $result['location']['name']);
+
+    }
+
+    public function testIndexError()
+    {
+        $weatherControllerMock = $this->createMock(WeatherController::class);
+
+        $request = $this->createMock(ServerRequestInterface::class);
+        $request->method('getQueryParams')->willReturn(['city' => 'Franca']);
+
+        $response = $this->createMock(ResponseInterface::class);
+
+        $weatherControllerMock->expects($this->once())
+            ->method('index')
+            ->willReturnCallback(function ($request, $response) {
+                return ['status' => 'error', 'message' => 'Error'];
+            });
+
+        $result = $weatherControllerMock->index($request, $response);
+
+        $this->assertEquals('error', $result['status']);
     }
 
 }
